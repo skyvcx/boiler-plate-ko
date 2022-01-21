b66a692629487ab27956dc94d08ae893614615f3
skyvcx <85545381+skyvcx@users.noreply.github.com>
2022-01-21 12:46:00 +0900

    2022-01-21 로그아웃기능 추가

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const bodyParser = require('body-parser');
 // package.json에 설치한 body-parser 모듈을 js로 불러들임
 const cookieParser = require('cookie-parser');
 // package.json에 설치한 cookie-parser 모듈을 js로 불러들임 
-const {User} = require("./models/User");
+
+const { auth } = require("./middleware/auth");
+const { User } = require("./models/User");
 // models/User.js를 불러옴
 const config = require('./config/key');
 
@@ -16,26 +18,25 @@ const config = require('./config/key');
 
 // 데이터를 분석하여가져올수있게 bodyParser설정을해줌
 //application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 //application/json
 app.use(bodyParser.json());
 app.use(cookieParser());
 
 const mongoose = require('mongoose')
-mongoose
+mongoose 
     .connect(config.mongoURI)
     .then(() => console.log("DB Connection Successfull!"))
     .catch((err) => {
-    console.log(err);
+        console.log(err);
     });
 
 
-app.get('/',(req,res) => res.send('Hello World!~~'))
+app.get('/', (req, res) => res.send('Hello World!~~'))
 // 루트 디렉토리에 들어올경우 res.send로 HelloWorld! 를 출력하도록함
 
-//유저정보를 저장하는 코드
-
-app.post('/register',(req,res)=>{
+//유저정보를 저장하는 코드(회원가입)
+app.post('/api/users/register', (req, res) => {
 
     //회원가입 할때 필요한 정보들을 클라이언트 에서 가져오면
     // 그것들을 데이터베이스에 넣어준다.
@@ -45,57 +46,79 @@ app.post('/register',(req,res)=>{
 
 
     // 몽고DB에서 오는 메서드
-    user.save((err,userInfo) =>{
-        if(err) return res.json({ success:false,err}) //만약 err가 있으면 json형식으로 성공하지 못했다는 이력과 err메시지를 같이 전송함
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err }) //만약 err가 있으면 json형식으로 성공하지 못했다는 이력과 err메시지를 같이 전송함
         return res.status(200).json({
-            success:true
+            success: true
         })//return end
     }) //user.save end
 })// app.post end
 
 // 유저정보를 저장하는 코드 종료
 
-//유저 로그인 코드
-app.post('/login',(req,res) => {
-    
-    //요청된 이메일을 데이터베이스에서 검색
-    User.findOne({ email: req.body.email},(err,user) =>{ // User플렉션 안에 요청된이메일값이 
-    
-        if(!user){  //한명도 없을경우
-            return res.json({
-                loginSuccess : false,  
-                message : "제공된 이메일에 해당하는 유저가 없습니다."
-            }) //해당데이터를 리턴
-            
-        }
-        
-        //요청된 이메일이 데이터베이스에 있으면 비밀번호가 일치하는지 검사
-        user.comparePassword(req.body.password,(err,isMatch)=>{
-            
-            if(!isMatch) // 요청값과 데이터베이스에 존재하는값이 일치하지 않을경우
-            return res.json({ loginSuccess:false,message:"비밀번호가 틀렸습니다." }) 
+//유저 로그인 코드(로그인)
+app.post('/api/users/login', (req, res) => {
 
-                        
+    //요청된 이메일을 데이터베이스에서 검색
+    User.findOne({ email: req.body.email }, (err, user) => { // User플렉션 안에 요청된이메일값이 
+
+        if (!user) {  //한명도 없을경우
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다."
+            }) //해당데이터를 리턴
+
+        }
+
+        //요청된 이메일이 데이터베이스에 있으면 비밀번호가 일치하는지 검사
+        user.comparePassword(req.body.password, (err, isMatch) => {
+
+            if (!isMatch) // 요청값과 데이터베이스에 존재하는값이 일치하지 않을경우
+                return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
+
+
             //비밀번호까지 일치할경우 토큰을 생성
-            user.generateToken((err,user)=>{
-                if(err) return res.status(400).send(err);
-    
-                 // 생성한 토큰을 저장한다 (쿠키에 저장)
-                res.cookie("x_auth",user.token)
+            user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err);
+
+                // 생성한 토큰을 저장한다 (쿠키에 저장)
+                res.cookie("x_auth", user.token)
                     .status(200)
-                    .json({ loginSuccess:true,userId:user._id })
-                }) //토큰 종료
+                    .json({ loginSuccess: true, userId: user._id })
+            }) //토큰 종료
         })
     }) //검색 종료
 })// 유저로그인코드 종료
 
 
+app.get('/api/users/auth', auth, (req, res) => {// 엔드포인트에서 auth는 콜백 (req,res)전에 동작
 
-    
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    })
+})
 
 
+app.get('/api/users/logout', auth, (req, res) => {
 
-app.listen(port,()=>console.log(`Example app listening on port ${port}!`))
+    User.findOneAndUpdate(
+        { _id: req.user._id },
+        { token: "" }, (err, user) => {
+            
+            if (err) return res.json({ success: false, err });
+            return res.status(200).send({
+                success: true
+            })
+        })
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 // app이 포트번호를 읽게되면 콘솔로그를 찍는다.
 
 
--- models/User.js
+++ models/User.js
@@ -1,97 +1,114 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');   
+const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
-    name:{
+    name: {
         type: String,
-        maxlength:50
+        maxlength: 50
 
     },
-    email:{
-        type:String,
-        trim:true, // sky vcx@naver.com 를 skyvcx@naver.com 로 변경해줌
-        unique:1
-    },
-    password:{
+    email: {
         type: String,
-        minlenght:5
+        trim: true, // sky vcx@naver.com 를 skyvcx@naver.com 로 변경해줌
+        unique: 1
     },
-    lastname:{
-        type:String,
-        maxlength:50
+    password: {
+        type: String,
+        minlenght: 5
     },
-    role:{ // 유저에게 권한을 부여하는 코드넘버
-        type:Number,
-        default:0
+    lastname: {
+        type: String,
+        maxlength: 50
     },
-    image:String,
-    token:{ // 유효성검사를위한 토큰
-        type:String
+    role: { // 유저에게 권한을 부여하는 코드넘버
+        type: Number,
+        default: 0
     },
-    tokenExp:{ // 토큰에 유효기간
+    image: String,
+    token: { // 유효성검사를위한 토큰
+        type: String
+    },
+    tokenExp: { // 토큰에 유효기간
 
     }
-    
+
 })
 
 
-userSchema.pre('save',function(next){
+userSchema.pre('save', function (next) {
     var user = this;
     //this는 userShema 가 mongoose.Schema 클래스로 생성되었고, Schema는 타입스크립트 클래스로 만들어졌으며, 타입스크립트에서 this는 해당 클래스를 동적으로 참조하는데 사용되기 때문입니다.
 
-    if(user.isModified('password')){ //userSchema 모델안에 password 만 변경되었을경우만 암호화를 한다.
-         // 1.비밀번호를 암호화 한다.
-    bcrypt.genSalt(saltRounds, function(err, salt) {
-        if(err) return next(err)
+    if (user.isModified('password')) { //userSchema 모델안에 password 만 변경되었을경우만 암호화를 한다.
+        // 1.비밀번호를 암호화 한다.
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) return next(err)
 
-        bcrypt.hash(user.password, salt, function(err, hash) {
-            //순수 password를 비크립트로 변환하고
-            if(err) return next(err) //에러 발생시 에러를 반환 해주고
-            user.password = hash // 성공하였을경우 hash에 담긴 비밀번호로 변경되어 next() 함수를 동작시킴
-            next()
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                //순수 password를 비크립트로 변환하고
+                if (err) return next(err) //에러 발생시 에러를 반환 해주고
+                user.password = hash // 성공하였을경우 hash에 담긴 비밀번호로 변경되어 next() 함수를 동작시킴
+                next()
+            })
         })
-    })
 
-    } else{ // 비밀번호를 바꾸는게 아니라 다른 것을 변경할경우 
+    } else { // 비밀번호를 바꾸는게 아니라 다른 것을 변경할경우 
         next()
     }
 })
-    // 유저정보를 저장하기전에 실행하게 만드는 코드
+// 유저정보를 저장하기전에 실행하게 만드는 코드
 
 //비밀번호 일치판단 코드
-userSchema.methods.comparePassword = function(plainPassword, cb){
+userSchema.methods.comparePassword = function (plainPassword, cb) {
 
     //plainPassword qwer1234  암호화된 비밀번호  비교코드 작성
-    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
 
-        if(err) return cb(err);
+        if (err) return cb(err);
         cb(null, isMatch)
 
     })
 }
 
 
-userSchema.methods.generateToken = function(cb){
+userSchema.methods.generateToken = function (cb) {
     //jsonwebtoken을 이용하여 토큰 생성
     var user = this;
-    
-    var token = jwt.sign(user._id.toHexString(),'secretToken')
+
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
     //user._id + 'secretToken' = token
     // 'secretToken' -> user._id
 
     user.token = token
     //유저토큰에 토큰을 넣어줌
-    user.save(function(err,user){
-        if(err) return cb(err) //만약 에러가발생하면 에러를 반환해주고
-        cb(null,user)// 에러가없으면 user 에담아 보냄
-    
+    user.save(function (err, user) {
+        if (err) return cb(err) //만약 에러가발생하면 에러를 반환해주고
+        cb(null, user)// 에러가없으면 user 에담아 보냄
+
     })
 }
 
-const User = mongoose.model('User',userSchema)
+userSchema.statics.findByToken = function (token, cb) {
+    var user = this;
+    //토큰을 Decode(복호화)
+    jwt.verify(token, 'secretToken', function (err, decoded) {
+        //유저 아이디를 이용해서 유저를 찾은후
+        //클라이언트에서 가져온 토큰과 DB에 보관된 토큰이 일치하는지 확인
+        user.findOne({ "_id": decoded, "token": token }, function (err, user) {
+
+            if (err) return cb(err);
+            cb(null, user)
+
+        })
+
+    })
+}
+
+
+const User = mongoose.model('User', userSchema)
 // 스프링 컨트롤러에서 모델에 변수넣는느낌 ?  model('변수명',변수에 들어갈내용)
 
-module.exports= {User}
+module.exports = { User }
 // 다른 파일에서 사용하기위한 exports 명칭
